f896bed64f8f3292fb6a0f459f132a549d7c9938
Strikeskids <github@strikeskids.com>
2014-02-05 08:27:46 -0500

    Testing new path

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 
+command('nc -zv google.com 80', {env: {'PATH':'/etc/alternatives/:/sbin'}}, console.log);
+command('/etc/alternatives/nc -zv google.com 80', console.log);
+command('/sbin/nc -zv google.com 80',console.log);
+
 app.engine('jade', engines.jade);
 app.use(logfmt.requestLogger());
 
@@ -26,7 +30,7 @@ io.sockets.on('connection', function(socket) {
 		console.log("Ping query", address);
 		var e = 'nc -zv '+address+' 80';
 		console.log('Executing',e)
-		var ping = command(e, {timeout: 5000, env: {'PATH':'/sbin'}}, function(error, stdout, stderr) {
+		var ping = command(e, {timeout: 5000, env: {'PATH':'/etc/alternatives/:/sbin'}}, function(error, stdout, stderr) {
 			console.log("Ping finished",address,error,stdout,stderr);
 			socket.emit('ipresp', error, stdout, stderr);
 		});
